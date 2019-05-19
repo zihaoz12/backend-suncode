4a88ef18e0dabadf88ccd51c6ca3ce9c34969f35
seheesf88 <seheesf88@gmail.com>
2019-05-18 17:05:03 -0700

    before ux

--- models/house.js
+++ models/house.js
@@ -8,7 +8,21 @@ const houseSchema = new mongoose.Schema({
   year: {type: String},
   sqft: {type: String},
   pic1: {type: String},
+  pic2: {type: String},
+  pic3: {type: String},
+  pic4: {type: String},
   memo: {type: String},
+  attic: {type: String},
+  whyear: {type: String},
+  whef: {type: String},
+  whfuel: {type: String},
+  heatyear: {type: String},
+  heatef: {type: String},
+  heatfuel: {type: String},
+  status: {type: String},
+  memo2: {type: String},
+  authorId: {type: String},
+  authorname: { type: String}
 });
 
 module.exports = mongoose.model('House', houseSchema)
